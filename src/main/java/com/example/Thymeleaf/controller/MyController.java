c0f796e46376dd22699fb72784c54ca992a16c5a
Ankur Singh <ankurrana231002@gmail.com>
2023-04-29 21:54:58 +0530

    Add Iterator

--- src/main/java/com/example/Thymeleaf/controller/MyController.java
+++ src/main/java/com/example/Thymeleaf/controller/MyController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class MyController {
@@ -17,6 +18,19 @@ public class MyController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("about");
         return modelAndView;
+    }
+
+    //handling iteration
+    @GetMapping("/loop")
+    public ModelAndView iteratorHandler(Model model){
+
+        List<String> nameOfList = List.of("ankur", "singh", "pundir", "ankit", "rahul");
+        model.addAttribute("names",nameOfList);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("iterator");
+        return modelAndView;
+
     }
 
 }
